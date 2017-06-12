81d0873505dd6fc6dcdb6714e81bce3070cf6c0b
Renato Lessa <renatolessa.2011@hotmail.com>
2017-06-12 11:32:14 -0300

    Criando Controller

--- src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
+++ src/main/java/br/com/fabricadeprogramador/persistencia/jdbc/ConexaoFactory.java
@@ -9,13 +9,12 @@ public class ConexaoFactory {
 	public static Connection getConnection() {
 
 		try {
+			Class.forName("org.postgresql.Driver");
 			return DriverManager.getConnection("jdbc:postgresql://localhost:5432/fabricaweb", "postgres", "123");
 		} catch (SQLException e) {
-
-			/**
-			 * Relançando exeção em caso de erro
-			 */
-
+			//releançando a exeception
+			throw new RuntimeException(e);
+		} catch (ClassNotFoundException e) {
 			throw new RuntimeException(e);
 		}
 	}
