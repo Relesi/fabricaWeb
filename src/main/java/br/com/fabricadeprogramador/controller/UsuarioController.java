f5a38c001e8032ed39c2174c444028ae9b6e0d42
Renato Lessa <renatolessa.2011@hotmail.com>
2017-06-12 17:11:48 -0300

    Configurando requisição - request, response e Listar

--- src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package br.com.fabricadeprogramador.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,45 +11,70 @@ import javax.servlet.http.HttpServletResponse;
 
 import br.com.fabricadeprogramador.persistencia.entidade.Usuario;
 import br.com.fabricadeprogramador.persistencia.jdbc.UsuarioDAO;
+
 //http://localhost:8080/fabricaweb/usucontroller.do
 @WebServlet("/usucontroller.do")
 public class UsuarioController extends HttpServlet {
-	
-	public UsuarioController(){
+
+	public UsuarioController() {
 		System.out.println("Construtor..");
 	}
+
 	@Override
 	public void init() throws ServletException {
 
 		System.out.println("Init..");
 		super.init();
 	}
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	
-		System.out.println("Chamou Get");
+		resp.setContentType("text/html");
+		String acao = req.getParameter("acao");
+		if (acao.equals("exc")) {
+			String id = req.getParameter("id");
+			Usuario usu = new Usuario();
+			if (id != null)
+				usu.setId(Integer.parseInt(id));
+
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			usuarioDAO.excluir(usu);
+
+			resp.getWriter().println("Excluído com Sucesso");
+		}else if(acao.equals("lis")){
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			List<Usuario> lista = usuarioDAO.buscarTodos();
+			
+			for(Usuario u: lista){
+				
+				resp.getWriter().println(u.getNome() + "<br>" );
+			}
+			
+		}
 	}
 
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
-			throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+		String id = req.getParameter("id");
 		String nome = req.getParameter("nome");
 		String login = req.getParameter("login");
 		String senha = req.getParameter("senha");
-		
+
 		Usuario usu = new Usuario();
+		if (id != null)
+			usu.setId(Integer.parseInt(id));
 		usu.setNome(nome);
 		usu.setLogin(login);
 		usu.setSenha(senha);
-		
+
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		usuarioDAO.salvar(usu);
-			
+
 		resp.getWriter().print("<b>Sucesso!</b>");
 
-
 	}
+
 	@Override
 	public void destroy() {
 
