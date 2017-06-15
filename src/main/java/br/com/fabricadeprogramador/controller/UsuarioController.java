2337409678ebbe30a3c24200bc97814d3aa84fc8
Renato Lessa <renatolessa.2011@hotmail.com>
2017-06-14 23:29:02 -0300

    Instanciando objeto alterar e cadastrar novo usuario

--- src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
+++ src/main/java/br/com/fabricadeprogramador/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.com.fabricadeprogramador.controller;
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -31,25 +32,45 @@ public class UsuarioController extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setContentType("text/html");
 		String acao = req.getParameter("acao");
+
 		if (acao.equals("exc")) {
 			String id = req.getParameter("id");
 			Usuario usu = new Usuario();
 			if (id != null)
 				usu.setId(Integer.parseInt(id));
-
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
 			usuarioDAO.excluir(usu);
 
-			resp.getWriter().println("Excluído com Sucesso");
-		}else if(acao.equals("lis")){
+			// resp.getWriter().println("Excluído com Sucesso");
+			resp.sendRedirect("usucontroller.do?acao=lis");
+
+		} else if (acao.equals("lis")) {
+
 			UsuarioDAO usuarioDAO = new UsuarioDAO();
 			List<Usuario> lista = usuarioDAO.buscarTodos();
-			
-			for(Usuario u: lista){
-				
-				resp.getWriter().println(u.getNome() + "<br>" );
-			}
-			
+			req.setAttribute("lista", lista);
+			RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/listausu.jsp");
+			dispatcher.forward(req, resp);
+
+		} else if (acao.equals("alt")) {
+
+			String id = req.getParameter("id");
+			UsuarioDAO usuarioDAO = new UsuarioDAO();
+			Usuario usuario = usuarioDAO.buscarPorId(Integer.parseInt(id));
+			req.setAttribute("usu", usuario);
+			RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/formusuario.jsp");
+			dispatcher.forward(req, resp);
+
+		} else if (acao.equals("cad")) {
+
+			Usuario usuario = new Usuario();
+			usuario.setId(0);
+			usuario.setNome("");
+			usuario.setLogin("");
+			usuario.setSenha("");
+			req.setAttribute("usu", usuario);
+			RequestDispatcher dispatcher = req.getRequestDispatcher("WEB-INF/formusuario.jsp");
+			dispatcher.forward(req, resp);
 		}
 	}
 
@@ -62,6 +83,7 @@ public class UsuarioController extends HttpServlet {
 		String senha = req.getParameter("senha");
 
 		Usuario usu = new Usuario();
+
 		if (id != null)
 			usu.setId(Integer.parseInt(id));
 		usu.setNome(nome);
